68687eebcaa07b8b5f6ea7463e83c1fa72515f87
Gaila <gaila.vastakaite@gmail.com>
2023-01-05 16:26:18 +0200

    add array of movie names and console log it

---
+++ arrays.js
@@ -0,0 +1,6 @@
+//Array of movies
+const arr = ["The Shawshank Redemption", "The Godfather", "The Dark Knight", "12 Angry Men", "The Lord Of The Rings"];
+
+for(i= 0; i<arr.length; i++){
+  console.log(arr[i]);
+}
