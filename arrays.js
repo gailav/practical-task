36c7b2d8bfc85b6f87fb1a0f265138a5db80ddbc
Gaila <gaila.vastakaite@gmail.com>
2023-01-05 17:13:17 +0200

    convert array to string and back

--- arrays.js
+++ arrays.js
@@ -3,4 +3,14 @@ const arr = ["The Shawshank Redemption", "The Godfather", "The Dark Knight", "12
 
 for(i= 0; i<arr.length; i++){
   console.log(arr[i]);
-}
+};
+
+// Convert array to string
+
+const arr1 = ["Toyota", "Audi", "Porche", "Tesla"];
+const toString = arr1.toString();
+
+console.log(toString);
+
+const toArray = toString.split(",");
+console.log(toArray);
