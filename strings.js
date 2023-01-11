610f01dc535e1429aafb38bf14e9ca3e21c8d717
Gaila <gaila.vastakaite@gmail.com>
2023-01-11 20:59:59 +0200

    write regex that finds 2+3

--- strings.js
+++ strings.js
@@ -3,3 +3,11 @@ const string = "ahb acb aeb aeeb adcb axeb";
 const re = /a\Sb/gi;
 
 console.log(string.match(re));
+
+//Write regex that finds 2 + 3
+
+const numString = "2 + 3 223 2223";
+
+const reg = /2\s\+\s3/gi;
+
+console.log(numString.match(reg));
