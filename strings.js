62a65005ec4673f5d789ef50e98e82ec061c0a97
Gaila <gaila.vastakaite@gmail.com>
2023-01-12 09:03:43 +0200

    get date and log it on different lines

--- strings.js
+++ strings.js
@@ -11,3 +11,15 @@ const numString = "2 + 3 223 2223";
 const reg = /2\s\+\s3/gi;
 
 console.log(numString.match(reg));
+
+//Get the date
+
+const today = new Date();
+
+const day = today.getDate();
+const month = today.getMonth();
+const year = today.getFullYear();
+
+console.log(day);
+console.log(month+1);
+console.log(year);
